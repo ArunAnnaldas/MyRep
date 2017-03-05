48f8d4097a62a54d77960e98f6128f0ed9feaace
ArunAnnaldas <cmeatarun1988@gmail.com>
2017-03-05 23:53:35 +0530

    Added FirefoxBrowser launch

--- src/multipleDataProviders/Login.java
+++ src/multipleDataProviders/Login.java
@@ -1,5 +1,7 @@
 package multipleDataProviders;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Guice;
 import org.testng.annotations.Test;
 
@@ -7,10 +9,24 @@ import org.testng.annotations.Test;
 public class Login {
 
 	@Test(dataProvider = "DataRepository", dataProviderClass = DataProviderHub.class)
-	public void testLogin(String UserName, String Password) {
+	public void testLogin(String UserName, String Password) throws InterruptedException {
 
 		System.out.println("Login Tested successfully with UserName : " + UserName + " & Password : " + Password
 				+ " with thread id : " + Thread.currentThread().getId());
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com/");
+		driver.navigate().to("https://www.facebook.com/");
+		Thread.sleep(5000);
+		driver.navigate().back();
+		Thread.sleep(5000);
+		driver.navigate().forward();
+		String url = driver.getCurrentUrl();
+		//String PageSource = driver.getPageSource();
+		System.out.println("Url is : " + url);
+		//System.out.println("Source is " + PageSource);
+		System.out.println(driver.getWindowHandle());
+		driver.quit();
 
 	}
 
